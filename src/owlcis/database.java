8d6e592c3fdda3d8a652171a5dd1269a9002d56f
Jeff <Jeff@Jeff-PC>
2016-02-28 14:02:57 -0500

    Complete server

---
+++ src/owlcis/database.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package owlcis;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jeff
+ */
+public class database {
+
+    private static Connection connection = null;
+
+    public static Connection getConn() throws SQLException {
+        /*
+         This method will intialize database connection for OWL CIS to backend.
+         */
+        if (connection != null) {
+            return connection;
+        } else {
+
+            String driver = "com.mysql.jdbc.Driver";
+            /* try for connection by passing location of mysql, user and
+             password to connect it. We will utlize getConnection method in drivermanager
+             class
+             */
+
+            /* catch all exceptions for connection */
+            return connection;
+        }
+
+    }
+
+    public static Connection close() throws SQLException {
+        //it will forcefully close any open connection in database.
+        return null;
+        /* forcefully close the open connection */
+    }
+
+}
