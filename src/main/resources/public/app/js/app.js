8297a7c29b81a73631cd08cfed69ead13d50399c
dhruvinsheth <dhruvin.sheth@temple.edu>
2016-03-08 09:54:20 -0500

    Finish Main and user web pages

---
+++ src/main/resources/public/app/js/app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dhruvin sheth
+ */
+
+var authApp = angular.module('authApp',['ui.router']);
+
+authApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/first');
+
+    $stateProvider
+
+        .state('first', {
+            url: '/first',
+            templateUrl: 'home.html',
+            controller: 'firstController'
+        }).state('second', {
+            url: '/second',
+            templateUrl: 'user.html',
+            controller:'secondController'
+        }).state('third', {
+            url: '/third',
+            templateUrl: 'course.html',
+            controller:'thirdController'
+        }).state('fourth', {
+            url: '/fourth',
+            templateUrl: 'schedule.html',
+            controller:'fourthController'
+        });
+
+});
