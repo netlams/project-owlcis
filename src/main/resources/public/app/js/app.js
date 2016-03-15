3e195e74e8ae01e051d3ad9f2d451727477159fc
dhruvinsheth <dhruvin.sheth@temple.edu>
2016-03-15 11:40:36 -0400

    Post review pages, still working on css to move stars to center.

--- src/main/resources/public/app/js/app.js
+++ src/main/resources/public/app/js/app.js
@@ -6,24 +6,24 @@ var authApp = angular.module('authApp',['ui.router']);
 
 authApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/first');
+    $urlRouterProvider.otherwise('/home');
 
     $stateProvider
 
         .state('first', {
-            url: '/first',
+            url: '/home',
             templateUrl: 'home.html',
             controller: 'firstController'
         }).state('second', {
-            url: '/second',
+            url: '/user',
             templateUrl: 'user.html',
             controller:'secondController'
         }).state('third', {
-            url: '/third',
+            url: '/course',
             templateUrl: 'course.html',
             controller:'thirdController'
         }).state('fourth', {
-            url: '/fourth',
+            url: '/schedule',
             templateUrl: 'schedule.html',
             controller:'fourthController'
         }).state('signup', {
